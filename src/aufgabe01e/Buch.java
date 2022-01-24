bf7a259a95a6d2be7f94d82fd2ae30e90eddbd6e
HerrIves <spechtator@gmail.com>
2022-01-24 15:23:48 +0100

    1d buch added

--- src/aufgabe01e/Buch.java
+++ src/aufgabe01e/Buch.java
@@ -1,2 +1,13 @@
-package aufgabe01e;public class Buch {
+package aufgabe01e;
+
+import java.util.Date;
+
+public class Buch {
+    String titel;
+    Isbn isbn;
+    Date datum;
+    String author;
+    int anzahlSeiten;
+
+    private class Isbn {}
 }

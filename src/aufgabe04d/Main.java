39f4ba82e1a375d1f5f7eee145fb2e6bc500ba34
HerrIves <spechtator@gmail.com>
2022-02-10 16:00:06 +0100

    04d slf4j added

--- src/aufgabe04d/Main.java
+++ src/aufgabe04d/Main.java
@@ -1,16 +1,21 @@
 package aufgabe04d;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import utils.InputHelper;
 
 public class Main {
+    static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
 
-        String password =
-                new PassGenerator()
-                        .setPassLength(InputHelper.readInt("Enter length of password"))
-                        .generateAndCheckPass();
-
-        System.out.println(password);
+        logger.info("Hello world");
+//
+//        String password =
+//                new PassGenerator()
+//                        .setPassLength(InputHelper.readInt("Enter length of password"))
+//                        .generateAndCheckPass();
+//
+//        System.out.println(password);
 
     }
 }

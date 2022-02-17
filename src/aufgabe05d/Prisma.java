146a9f0407ef44f493035ca32cfc538a6cd9461c
HerrIves <spechtator@gmail.com>
2022-02-17 15:03:36 +0100

    5d agefangen mit prisma

--- src/aufgabe05d/Prisma.java
+++ src/aufgabe05d/Prisma.java
@@ -1,21 +1,6 @@
 package aufgabe05d;
 
 public abstract class Prisma implements Form{
-    private double length;
     abstract double getBaseArea();
     abstract double getBasePerimetr();
-
-    public Prisma(double length) {
-        this.length = length;
-    }
-
-    @Override
-    public double getSurfaceArea() {
-        return 2*getBaseArea() + getBasePerimetr() * length;
-    }
-
-    @Override
-    public double getVolume() {
-        return getBaseArea() * length;
-    }
 }

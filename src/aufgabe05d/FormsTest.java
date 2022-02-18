b4b93c7fc233a48c9b3985b6339ac84c2dd70c99
HerrIves <spechtator@gmail.com>
2022-02-18 16:26:33 +0100

    05d quader finished, testen  added

---
+++ src/aufgabe05d/FormsTest.java
@@ -0,0 +1,31 @@
+package aufgabe05d;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FormsTest {
+    Quader quader = new Quader(4, 5, 10);
+
+    @Test
+    void testGetSurfaceArea() {
+        assertEquals(220, quader.getSurfaceArea());
+    }
+
+    @Test
+    void getVolume() {
+        assertEquals(200, quader.getVolume());
+    }
+
+    @Test
+    void getBaseArea() {
+        assertEquals(20, quader.getBaseArea());
+    }
+
+    @Test
+    void getBasePerimetr() {
+        assertEquals(18, quader.getBasePerimetr());
+    }
+}
--- src/aufgabe05d/Quader.java
+++ src/aufgabe05d/Quader.java
@@ -1,5 +1,22 @@
 package aufgabe05d;
 
-public class Quader{
+public class Quader extends Prisma{
+    double width;
+    double height;
 
+    public Quader(double width, double height, double length) {
+        super(length);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    double getBaseArea() {
+        return width * height;
+    }
+
+    @Override
+    double getBasePerimetr() {
+        return 2 * (width + height);
+    }
 }

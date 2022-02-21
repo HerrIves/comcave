c3b56ddc1cf3e79f194be64d12a0945a4649ca16
HerrIves <spechtator@gmail.com>
2022-02-21 17:42:39 +0100

    Quader.printInfo() finished

--- src/aufgabe05d/Form.java
+++ src/aufgabe05d/Form.java
@@ -3,5 +3,10 @@ package aufgabe05d;
 public interface Form {
     double getSurfaceArea();
     double getVolume();
+
+    default void printInfo() {
+        System.out.println("Surface area:" + getSurfaceArea());
+        System.out.println("Volume: " + getVolume());
+    }
 }
 
